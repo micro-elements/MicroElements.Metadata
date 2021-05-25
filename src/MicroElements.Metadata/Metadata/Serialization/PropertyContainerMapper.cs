26a64abdef0ebf6ddede35fc6bcb4c502f31ffbc
Alexey Petryashev <petriashev@gmail.com>
2021-05-25 23:20:48 +0300

    ToPropertyContainer customization and nullable search support

--- src/MicroElements.Metadata/Metadata/Serialization/PropertyContainerMapper.cs
+++ src/MicroElements.Metadata/Metadata/Serialization/PropertyContainerMapper.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using MicroElements.Functional;
@@ -62,17 +63,26 @@ namespace MicroElements.Metadata.Serialization
         /// <typeparam name="TSchema">Schema.</typeparam>
         /// <param name="sourceData">Source object.</param>
         /// <param name="includePropertiesNotFromSchema">Should include properties not from schema.</param>
+        /// <param name="propertyComparer">Optional property comparer for search property in schema. Default: <see cref="PropertyComparer.ByTypeAndNameIgnoreCaseComparer"/>.</param>
+        /// <param name="propertyFactory">Optional property factory.</param>
+        /// <param name="propertyValueFactory">Optional property value factory.</param>
         /// <returns>Result property container.</returns>
-        public static PropertyContainer<TSchema> ToPropertyContainer<TSchema>(this object sourceData, bool includePropertiesNotFromSchema = false)
+        public static PropertyContainer<TSchema> ToPropertyContainer<TSchema>(
+            this object sourceData,
+            bool includePropertiesNotFromSchema = false,
+            IEqualityComparer<IProperty>? propertyComparer = null,
+            IPropertyFactory? propertyFactory = null,
+            IPropertyValueFactory? propertyValueFactory = null)
             where TSchema : IPropertySet, new()
         {
-            var propertyComparer = PropertyComparer.ByTypeAndNameIgnoreCaseComparer;
-            var searchOptions = SearchOptions.Default.WithPropertyComparer(propertyComparer);
-            IPropertyFactory propertyFactory = new CachedPropertyFactory();
-            IPropertyValueFactory propertyValueFactory = new CachedPropertyValueFactory(new PropertyValueFactory(), propertyComparer);
+            propertyComparer ??= PropertyComparer.ByTypeAndNameIgnoreCaseComparer;
+            propertyFactory ??= new CachedPropertyFactory();
+            propertyValueFactory ??= new CachedPropertyValueFactory(new PropertyValueFactory(), propertyComparer);
 
             var schema = new TSchema();
             IProperty[] schemaProperties = schema.ToArray();
+
+            var searchOptions = SearchOptions.Default.WithPropertyComparer(propertyComparer);
             var propertyContainer = new MutablePropertyContainer(searchOptions: searchOptions);
 
             PropertyInfo[] propertyInfos = sourceData.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
@@ -82,6 +92,13 @@ namespace MicroElements.Metadata.Serialization
 
                 IProperty propertyFromType = propertyFactory.Create(propertyInfo.PropertyType, propertyInfo.Name);
                 IProperty? schemaProperty = schemaProperties.FirstOrDefault(property => propertyComparer.Equals(property, propertyFromType));
+
+                // propertyInfo.PropertyType can be Nullable but property in schema can be not nullable
+                if (schemaProperty == null && value != null && value.GetType() != propertyInfo.PropertyType)
+                {
+                    propertyFromType = propertyFactory.Create(value.GetType(), propertyInfo.Name);
+                    schemaProperty = schemaProperties.FirstOrDefault(property => propertyComparer.Equals(property, propertyFromType));
+                }
 
                 IProperty? propertyToAdd = schemaProperty;
                 if (schemaProperty == null && includePropertiesNotFromSchema)
